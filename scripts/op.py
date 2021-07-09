118f0a20b380f9f0f7fdf7735cea93b5e17934d2
Breno Martins <crdj161@gmail.com>
2021-07-09 11:14:44 -0300

    Sistema de OP

---
+++ scripts/op.py
@@ -0,0 +1,15 @@
+import json
+from sys import argv as args
+from json import load, dumps
+
+try:
+    jsonfile = load(open(f"info/{args[1]}.json", "r"))
+    jsonfile["type"] = "staff"
+    jsonfile["staff_type"] = args[2]
+    open(f"info/{args[1]}.json", "w+").write(dumps(jsonfile))
+except FileNotFoundError:
+    staffjson = {
+        "type": "staff",
+        "staff_type": f"{args[2]}"
+    }
+    open(f"info/{args[1]}.json", "w+").write(dumps(staffjson))
